c2e7c1d6f91207440f983baddbf93505bbae66f7
telgar <telgar@sportingindex.com>
2017-08-05 09:35:38 +0100

    Cache busting and removed dist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  cacheBuster: string ;
+
+  constructor() {
+
+    this.cacheBuster = new Date().getTime().toString();
+  }
 }
